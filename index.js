f353cc82912071b2c26c42dc2c5c9cc770f736bb
28111600 <28111600@qq.com>
2017-03-03 01:14:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var tty = require('./lib/tty.js');                                                                          
-                                                                                                      
-var app = tty.createServer({                                                                          
-  shell: 'bash',                                                                                      
-  shellArgs: [path.join(process.cwd() , "login.sh")],                                   
-  port: 8080                                                                                          
-});                                                                                                   
-                                                                                                      
-app.get('/foo', function(req, res, next) {                                                            
-  res.send('bar');                                                                                    
-});                                                                                                   
-                                                                                                      
-app.listen(); 
+var tty = require('./lib/tty.js');
+var path = require("path");
+var app = tty.createServer({
+    shell: 'sh',
+    shellArgs: [path.join(process.cwd(), "login.sh")],
+    port: 8080
+});
+
+app.get('/foo',
+function(req, res, next) {
+    res.send('bar');
+});
+
+app.listen();
