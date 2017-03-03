2f6e7dfb85ff4baedcb1b36a8a046a283ebb138b
28111600 <28111600@qq.com>
2017-03-03 22:12:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var tty = require('./lib/tty.js');
 var path = require("path");
 var app = tty.createServer({
-    shell: 'sh',
+    shell: 'bash',
     shellArgs: [path.join(process.cwd(), "login.sh")],
-    port: 8080
+    port: 8080,
+    term: {
+        geometry: [90, 30]
+    }
 });
 
 app.get('/foo',
-function(req, res, next) {
-    res.send('bar');
-});
+    function(req, res, next) {
+        res.send('bar');
+    });
 
 app.listen();
