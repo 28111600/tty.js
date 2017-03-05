72a1272824240ec374f202c0f5d9c1bb374ba2e9
xiaodong <28111600@qq.com>
2017-03-05 16:06:34 +0800

    Changes to be committed: 	modified:   index.js 	modified:   static/index.html 	modified:   static/style.css 	modified:   static/tty.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var tty = require('./lib/tty.js');
 var path = require("path");
 var app = tty.createServer({
-    shell: 'bash',
-    shellArgs: [path.resolve(__dirname, "login.sh")],
-    port: 8080,
-    term: {
-        geometry: [90, 30]
-    }
+	shell: 'bash',
+	shellArgs: [path.resolve(__dirname, "login.sh")],
+	port: 8080,
+	term: {
+		geometry: [90, 30]
+	}
 });
 
 app.get('/foo',
-    function(req, res, next) {
-        res.send('bar');
-    });
+		function(req, res, next) {
+			res.send('bar');
+		});
 
 app.listen();
