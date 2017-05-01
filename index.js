a157920d8563f5d82bebbd8e9d6d50e8626b9eb7
root <root@TENCENT.site>
2017-05-01 19:45:48 +0800

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var tty = require('./lib/tty.js');
 var path = require("path");
 var app = tty.createServer({
-    shell: 'bash',
-    shellArgs: [path.resolve(__dirname, "login.sh")],
-    port: 8080,
-    term: {
-        geometry: [80, 25],
-        cursorBlink: false
-    }
+	shell: 'bash',
+	shellArgs: [path.resolve(__dirname, "login.sh")],
+	port: 8080,
+	term: {
+		geometry: [80, 25],
+		cursorBlink: false
+	},
+	https:{
+		key:path.resolve(__dirname,"key.pem"),
+		cert:path.resolve(__dirname,"cert.pem")
+	}
 });
 
 app.listen();
